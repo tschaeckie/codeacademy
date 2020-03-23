5c54988118eebda117635c8965a2ce48155aef3c
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-03-23 15:32:01 +0100

    string interpolation

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-var favoriteAnimal = 'turtle';
-console.log('My favorite Animal: ' + favoriteAnimal);
+let myName = 'Jacqueline';
+let myCity = 'Rio de Janeiro';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
+/* Warum reicht es hier nicht, wie im Kapitel davor, die Variablen mit einem + zu verbinden? */
